e2e3154d6c6e791c4432744e49cf888c509bd7b1
BrasilBlackhat <contato@brasilblackhat.com.br>
2016-09-02 17:14:16 -0300

    Configurando Banco de Dados!

--- aula2/config/db.php
+++ aula2/config/db.php
@@ -4,6 +4,8 @@ $host = 'localhost';
 $usuario = 'root';
 $senha = '';
 $db = 'cursophp';
-$conexao = mysql_connect($host,$usuario,$senha)or die('ERROR GERAL');
-echo "<center>\nConecçao Estabelecida com banco de dados</center>";
+$conexao = mysql_connect($host,$usuario,$senha)or die(mysql_error());
+#echo "<center>\nConecçao Estabelecida com Sucesso!</center>";
+$selectBanco = mysql_select_db($db) or die(mysql_error());
+#echo "<center>Banco De Dados Logado! ";
 ?>
