7c4f7e9d19308600c7d19ecacb85da80666a9b24
BrasilBlackhat <contato@brasilblackhat.com.br>
2016-09-03 02:44:34 -0300

    2:44 da matina tentando criar ainda :)

--- aula2/index.php
+++ aula2/index.php
@@ -3,19 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title>Criando Meu sistema de Agenda!</title>
-    <link href="css/style.css" type="text/stylesheet">
+    <link rel="stylesheet" href="./css/style.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <h1> Lista de Tarefas Lucas Oliveira</h1>
     <div class="formulario">
-      <form class="formTarefa"
-      action="savetarefa.php" method="post">
+      <form class="formTarefa" action="savetarefa.php" method="post">
       <label for="tarefa">Tarefa</label>
-      <input type="text" id="tarefa"
-       name="name" value="">
-       <input type="submit" name="name" value="Enviar">
+      <input type="text" id="tarefa" name="tarefa" value="">
+      <input type="submit" value="Enviar">
       </form>
     </div>
-
+    <div class="tarefas">
+      <ul>
+        <?php  ?>
+        <li>
+          <span> Tarefa <?php ?></span>
+          <a href="#"class="butao btn-sucess">Editar</a>
+          <a href="#"class="butao btn-excluir">Remover</a>
+        </li>
+      </ul>
+     <?php  ?>
+    </div>
   </body>
 </html>
