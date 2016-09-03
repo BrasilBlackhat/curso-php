bbf3e8e9727e4c7dbb8a45146a0c4f8b7a944dfc
BrasilBlackhat <contato@brasilblackhat.com.br>
2016-09-02 23:56:07 -0300

    Criando Index

---
+++ aula2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Criando Meu sistema de Agenda!</title>
+    <link href="css/style.css" type="text/stylesheet">
+  </head>
+  <body>
+    <h1> Lista de Tarefas Lucas Oliveira</h1>
+    <div class="formulario">
+      <form class="formTarefa"
+      action="savetarefa.php" method="post">
+      <label for="tarefa">Tarefa</label>
+      <input type="text" id="tarefa"
+       name="name" value="">
+       <input type="submit" name="name" value="Enviar">
+      </form>
+    </div>
+
+  </body>
+</html>
